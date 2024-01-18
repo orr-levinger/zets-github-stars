55a689f6c13b6c91373e35c983e8b07090ec20e5
orrlevinger <mr.levinger@gmail.com>
2024-01-18 21:05:53 +0200

    clean code

--- backend/src/functions/github/save-repos.ts
+++ backend/src/functions/github/save-repos.ts
@@ -16,10 +16,13 @@ export const handler = async (event: any, context: Context) => {
     console.log('items:', event);
     return {
       headers: {
-        'Access-Control-Allow-Origin': '*', // Or a specific domain for production
-        'Access-Control-Allow-Headers': 'Content-Type',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*', // Replace with specific domain in production
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, x-requested-with', // Add other needed headers
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS', // Add other needed methods
       },
       statusCode: 201,
+      body: JSON.stringify({ message: 'Success' }), // Your response body
     };
   } catch (err) {
     return httpError(err, err.statusCode || 500);
