40f0ce586b9f8730567452d4e47c9598433318c8
orrlevinger <mr.levinger@gmail.com>
2024-01-20 13:57:14 +0200

    make frontend website automatically

--- frontend/src/aws-exports.js
+++ frontend/src/aws-exports.js
@@ -1,22 +1,23 @@
 import { Auth } from 'aws-amplify';
+console.log('REACT_APP_TEST', process.env.REACT_APP_TEST);
 const awsmobile = {
   aws_project_region: 'us-east-1',
   aws_cognito_region: 'us-east-1',
-  aws_user_pools_id: 'us-east-1_VVyEKTtDJ',
-  aws_user_pools_web_client_id: '3llcoknaubstls8mtl2lgs67gv',
+  aws_user_pools_id: process.env.REACT_APP_CognitoUserPoolId,
+  aws_user_pools_web_client_id: process.env.REACT_APP_CognitoWebClientId,
   aws_appsync_region: 'us-east-1',
   aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
   Auth: {
     identityPoolRegion: 'us-east-1',
-    userPoolId: 'us-east-1_VVyEKTtDJ',
+    userPoolId: process.env.REACT_APP_CognitoUserPoolId,
     region: 'us-east-1',
-    userPoolWebClientId: '3llcoknaubstls8mtl2lgs67gv',
+    userPoolWebClientId: process.env.REACT_APP_CognitoWebClientId,
   },
   API: {
     endpoints: [
       {
         name: 'ZetsAPIGatewayAPI',
-        endpoint: 'https://9enycjhcti.execute-api.us-east-1.amazonaws.com/dev',
+        endpoint: process.env.REACT_APP_ServiceEndpoint,
         custom_header: async () => {
           let authorization = `${(await Auth.currentSession()).getIdToken().getJwtToken()}`;
           return {
