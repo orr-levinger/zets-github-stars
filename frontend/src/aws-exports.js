d1d847869f5e892e9d2fb9e2a38ffa74f5c49cd8
orrlevinger <mr.levinger@gmail.com>
2024-01-22 15:46:36 +0200

    clean the code

--- frontend/src/aws-exports.js
+++ frontend/src/aws-exports.js
@@ -2,13 +2,13 @@ import { Auth } from 'aws-amplify';
 const awsmobile = {
   aws_project_region: 'us-east-1',
   aws_cognito_region: 'us-east-1',
-  aws_user_pools_id: process.env.REACT_APP_CognitoUserPoolId,
+  aws_user_pools_id: process.env.REACT_APP_CognitoUserPool2Id,
   aws_user_pools_web_client_id: process.env.REACT_APP_CognitoWebClientId,
   aws_appsync_region: 'us-east-1',
   aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
   Auth: {
     identityPoolRegion: 'us-east-1',
-    userPoolId: process.env.REACT_APP_CognitoUserPoolId,
+    userPoolId: process.env.REACT_APP_CognitoUserPool2Id,
     region: 'us-east-1',
     userPoolWebClientId: process.env.REACT_APP_CognitoWebClientId,
   },
